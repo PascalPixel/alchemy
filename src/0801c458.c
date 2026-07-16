12c9012b40e9819a7bae632abb493b8b00d1fc99
Pascal Pixel <x@pascalpixel.com>
2026-07-16 19:48:40 +0100

    ☀️ 1% – recover eight more exact functions

---
+++ src/0801c458.c
@@ -0,0 +1,18 @@
+typedef signed char s8;
+typedef unsigned char u8;
+typedef signed short s16;
+typedef unsigned short u16;
+typedef signed int s32;
+typedef unsigned int u32;
+typedef signed long long s64;
+typedef unsigned long long u64;
+typedef int bool;
+#define NULL ((void *)0)
+#define M2C_FIELD(base, type, offset)     (*(type)((u8 *)(base) + (offset)))
+
+s32 Func_08077048(s32);
+
+s32 Func_0801c458(s32 arg0, s32 arg1) {
+    Func_08077048(arg1);
+    return 0;
+}

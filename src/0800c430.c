4b809df7bd63f5e01116d1f0beb51fb6383a3658
Pascal Pixel <x@pascalpixel.com>
2026-07-16 17:34:31 +0100

    ☀️ 1% – define completion and resolve five byte matches

---
+++ src/0800c430.c
@@ -0,0 +1,22 @@
+typedef signed char s8;
+typedef unsigned char u8;
+typedef signed short s16;
+typedef unsigned short u16;
+typedef signed int s32;
+typedef unsigned int u32;
+typedef signed long long s64;
+typedef unsigned long long u64;
+typedef int bool;
+#define NULL ((void *)0)
+#define M2C_FIELD(base, type, offset)     (*(type)((u8 *)(base) + (offset)))
+
+s32 Func_0800b8ac(s32);
+
+void Func_0800c430(void *arg0, s32 arg1) {
+    if ((arg0 != NULL) && ((0xF & M2C_FIELD(arg0, u8 *, 0x54)) == 1)) {
+        arg0 = M2C_FIELD(arg0, void **, 0x50);
+        if (arg1 >= 0) {
+            Func_0800b8ac((s32) arg0);
+        }
+    }
+}

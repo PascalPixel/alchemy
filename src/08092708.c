c2bcd9c3a6b848169c6afb7eaf4207325859be5d
Pascal Pixel <x@pascalpixel.com>
2026-07-21 23:59:43 +0100

    decompile: clarify 08092708 sequence logic [912 of 2,203]

--- src/08092708.c
+++ src/08092708.c
@@ -1,14 +1,14 @@
-typedef signed char s8;
-typedef unsigned char u8;
-typedef signed short s16;
-typedef unsigned short u16;
 typedef signed int s32;
 typedef unsigned int u32;
-typedef signed long long s64;
-typedef unsigned long long u64;
-typedef int bool;
-#define NULL ((void *)0)
-#define M2C_FIELD(base, type, offset)     (*(type *)((u8 *)(base) + (offset)))
+typedef unsigned char u8;
+
+#define FIELD_S32(base, offset) (*(s32 *)((u8 *)(base) + (offset)))
+#define OBJECT_X(object) FIELD_S32(object, 0x08)
+#define OBJECT_Y(object) FIELD_S32(object, 0x0C)
+#define OBJECT_Z(object) FIELD_S32(object, 0x10)
+#define OBJECT_TARGET_Y(object) FIELD_S32(object, 0x14)
+#define OBJECT_VERTICAL_STEP(object) FIELD_S32(object, 0x28)
+#define OBJECT_MIRRORED_Y(object) FIELD_S32(object, 0x3C)
 
 void *Func_08092054(void);
 void Func_080f9010(s32);
@@ -20,54 +20,60 @@ void Func_08092624(void *, s32);
 void Func_08009158(void *);
 void Func_0809202c(void);
 
-void Func_08092708(s32 arg0, s32 arg1, s32 arg2) {
-    s32 temp_r3;
-    s32 temp_r9;
-    void *temp_r0;
-    u8 *ptr55;
+void Func_08092708(s32 sequence_arg, s32 mode_or_frame, s32 optional_action) {
+    s32 next_y;
+    s32 base_z;
+    void *object;
+    u8 *object_flags;
 
-    temp_r0 = Func_08092054();
-    temp_r9 = M2C_FIELD(temp_r0, s32, 0x10);
-    if (temp_r0 != NULL) {
+    object = Func_08092054();
+    base_z = OBJECT_Z(object);
+    if (object != 0) {
         Func_080f9010(0x121);
-        Func_08009080(temp_r0, arg1);
-        Func_080030f8(0xAU);
-        ptr55 = (u8 *) temp_r0 + 0x55;
-        Func_08009080(temp_r0, 1);
-        { u8 v55 = 2; v55 |= *ptr55; *ptr55 = v55; }
-        M2C_FIELD(temp_r0, s32, 0x28) = 0x40000;
-        Func_08009150(temp_r0, M2C_FIELD(temp_r0, s32, 8), M2C_FIELD(temp_r0, s32, 0xC), temp_r9 + 0xC0000);
-        Func_080030f8(6U);
+        Func_08009080(object, mode_or_frame);
+        Func_080030f8(10);
+        object_flags = (u8 *)object + 0x55;
+        Func_08009080(object, 1);
+        {
+            u8 flags = 2;
+            flags |= *object_flags;
+            *object_flags = flags;
+        }
+        OBJECT_VERTICAL_STEP(object) = 0x40000;
+        Func_08009150(object, OBJECT_X(object), OBJECT_Y(object),
+            base_z + 0xC0000);
+        Func_080030f8(6);
         Func_080f9010(0xD9);
-        arg1 = 0;
-        Func_08092adc(arg0, 0x5000, 0);
-        *ptr55 = 0U;
+        mode_or_frame = 0;
+        Func_08092adc(sequence_arg, 0x5000, 0);
+        *object_flags = 0;
         do {
-            temp_r3 = M2C_FIELD(temp_r0, s32, 0xC) + 0xFFFE0000;
-            M2C_FIELD(temp_r0, s32, 0xC) = temp_r3;
-            M2C_FIELD(temp_r0, s32, 0x3C) = temp_r3;
-            Func_080030f8(1U);
-            if ((arg2 != -1) && (1 & arg1)) {
-                Func_08092624(temp_r0, arg2);
+            next_y = OBJECT_Y(object) + 0xFFFE0000;
+            OBJECT_Y(object) = next_y;
+            OBJECT_MIRRORED_Y(object) = next_y;
+            Func_080030f8(1);
+            if ((optional_action != -1) && (mode_or_frame & 1)) {
+                Func_08092624(object, optional_action);
             }
-            arg1 += 1;
-        } while ((u32) arg1 <= 0xDU);
-        *ptr55 = 3U;
-        M2C_FIELD(temp_r0, s32, 0x28) = 0x30000;
-        Func_08009150(temp_r0, M2C_FIELD(temp_r0, s32, 8), M2C_FIELD(temp_r0, s32, 0xC), temp_r9 + 0x100000);
-        Func_08009158(temp_r0);
-        arg1 = 0;
-        if ((s32) M2C_FIELD(temp_r0, s32, 0xC) > (s32) M2C_FIELD(temp_r0, s32, 0x14)) {
-loop_7:
-            Func_080030f8(1U);
-            arg1 += 1;
-            if ((u32) arg1 <= 0xB3U) {
-                if ((s32) M2C_FIELD(temp_r0, s32, 0xC) > (s32) M2C_FIELD(temp_r0, s32, 0x14)) {
-                    goto loop_7;
+            mode_or_frame++;
+        } while ((u32)mode_or_frame <= 0xD);
+        *object_flags = 3;
+        OBJECT_VERTICAL_STEP(object) = 0x30000;
+        Func_08009150(object, OBJECT_X(object), OBJECT_Y(object),
+            base_z + 0x100000);
+        Func_08009158(object);
+        mode_or_frame = 0;
+        if (OBJECT_Y(object) > OBJECT_TARGET_Y(object)) {
+wait_for_target_y:
+            Func_080030f8(1);
+            mode_or_frame++;
+            if ((u32)mode_or_frame <= 0xB3) {
+                if (OBJECT_Y(object) > OBJECT_TARGET_Y(object)) {
+                    goto wait_for_target_y;
                 }
             }
         }
-        Func_080030f8(2U);
+        Func_080030f8(2);
         Func_0809202c();
     }
 }

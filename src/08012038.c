7900abb33272b723362d41cb6b72bd4313d37950
Pascal Pixel <x@pascalpixel.com>
2026-07-27 19:03:51 +0100

    ☀️ 8% – map cleanup

--- src/08012038.c
+++ src/08012038.c
@@ -1,21 +1,19 @@
 #include "types.h"
-
-#define M2C_FIELD(base, type, offset)     (*(type *)((u8 *)(base) + (offset)))
-
+#include "map.h"
 
 u8 Func_08012038(s32 arg0, s32 arg1, s32 arg2) {
-    s32 temp_r0;
+    struct MapState *temp_r0;
     s32 var_r2;
     s32 idx;
 
-    temp_r0 = *(s32 *)0x03001E70;
+    temp_r0 = *(struct MapState **)0x03001E70;
     arg1 >>= 0x14;
     arg2 >>= 0x14;
     var_r2 = 0x02010000;
     if (temp_r0 != 0) {
         idx = ((arg0 & 3) * 0x30) + 0x130;
-        var_r2 = *(s32 *)(temp_r0 + idx);
+        var_r2 = *(s32 *)((u8 *)temp_r0 + idx);
     }
     var_r2 += (arg1 + (arg2 << 7)) * 4;
-    return M2C_FIELD(var_r2, u8 *, 2);
+    return *(u8 *)(var_r2 + 2);
 }

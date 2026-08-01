f6e68ecf4b8bf953cf98420dc4a525f2fbbea159
Pascal Pixel <x@pascalpixel.com>
2026-08-01 23:11:50 +0100

    ☀️ 20% – decomp: promote exact 08079418 gap

--- src/08079418.c
+++ src/08079418.c
@@ -3,13 +3,18 @@ typedef signed int s32;
 typedef unsigned int u32;
 extern u8 Data_02000040[];
 
-s32 Func_08079418(u32 value, u32 shift)
+s32 Func_08079418(u32 value)
 {
     u32 shifted = value << 20;
+    u32 shift;
+    u32 mask;
     s32 result;
+
     shift = value & 4;
+    mask = 15;
     value = shifted >> 23;
     result = Data_02000040[value];
-    result &= 15 << shift;
+    result &= mask << shift;
     return result >> shift;
 }
+

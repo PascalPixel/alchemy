c30a0962d4462a0a1118d290a3831d24557363d5
Pascal Pixel <x@pascalpixel.com>
2026-08-09 20:54:34 +0100

    ☀️ 26% – exact: adopt duplicate overlay families

--- assets/code/resource_3bf_overlay.s
+++ assets/code/resource_3bf_overlay.s
@@ -7464,117 +7464,16 @@ AlchemyC_02005de4:
 	.space 0x2
 AlchemyC_02005df4:
 	.space 0x10
-	push	{r4, r5, r6, r7, lr}
-	ldr	r4, [r0, #4]
+AlchemyC_02005e04:
+	.space 0x4
 .L_02005e08:
-	sub	sp, #8
-	mov	r2, sp
-	str	r4, [r2, #0]
-	ldr	r3, [r0, #0]
-	str	r3, [r2, #4]
-	lsls	r3, r3, #12
-	lsrs	r6, r3, #12
-	ldrh	r3, [r2, #6]
-	lsls	r3, r3, #17
-	adds	r7, r1, #0
-	lsrs	r1, r3, #21
-	ldrb	r3, [r2, #7]
+	.space 0x18
 .L_02005e20:
-	lsrs	r3, r3, #7
-	adds	r5, r4, #0
-	str	r3, [r7, #4]
-	cmp	r1, #0
-	bne.n	.L_02005e70
-	adds	r3, r4, #0
-	orrs	r3, r6
-	cmp	r3, #0
-	bne.n	.L_02005e38
-	movs	r3, #2
-	str	r3, [r7, #0]
-	b.n	.L_02005ebc
+	.space 0x18
 .L_02005e38:
-	ldr	r3, [pc, #132]
-	lsrs	r1, r5, #24
-	lsls	r2, r6, #8
-	adds	r4, r1, #0
-	str	r3, [r7, #8]
-	orrs	r4, r2
-	lsls	r3, r5, #8
-	adds	r6, r4, #0
-	adds	r5, r3, #0
+	.space 0x12
 .L_02005e4a:
-	movs	r3, #3
-	str	r3, [r7, #0]
-	ldr	r3, [pc, #116]
-	cmp	r6, r3
-	bhi.n	.L_02005e98
-	mov	ip, r3
-.L_02005e56:
-	lsrs	r1, r5, #31
-	lsls	r2, r6, #1
-	adds	r4, r1, #0
-	lsls	r3, r5, #1
-	orrs	r4, r2
-	adds	r6, r4, #0
-	adds	r5, r3, #0
-	ldr	r3, [r7, #8]
-	subs	r3, #1
-	str	r3, [r7, #8]
-	cmp	r6, ip
-	bls.n	.L_02005e56
-	b.n	.L_02005e98
-.L_02005e70:
-	ldr	r2, [pc, #84]
-	cmp	r1, r2
-	bne.n	.L_02005e9e
-	adds	r3, r4, #0
-	orrs	r3, r6
-	cmp	r3, #0
-	bne.n	.L_02005e84
-	movs	r3, #4
-	str	r3, [r7, #0]
-	b.n	.L_02005ebc
-.L_02005e84:
-	movs	r2, #128
-	lsls	r2, r2, #12
-	adds	r4, r6, #0
-	movs	r3, #0
-	ands	r4, r2
-	orrs	r3, r4
-	cmp	r3, #0
-	beq.n	.L_02005e96
-	movs	r3, #1
-.L_02005e96:
-	str	r3, [r7, #0]
-.L_02005e98:
-	str	r5, [r7, #12]
-	str	r6, [r7, #16]
-	b.n	.L_02005ebc
-.L_02005e9e:
-	ldr	r2, [pc, #44]
-	adds	r3, r1, r2
-	lsrs	r1, r5, #24
-	lsls	r2, r6, #8
-	adds	r4, r1, #0
-	orrs	r4, r2
-	ldr	r1, [pc, #36]
-	ldr	r2, [pc, #36]
-	str	r3, [r7, #8]
-	movs	r3, #3
-	str	r3, [r7, #0]
-	orrs	r4, r2
-	lsls	r3, r5, #8
-	str	r3, [r7, #12]
-	str	r4, [r7, #16]
-.L_02005ebc:
-	add	sp, #8
-	pop	{r4, r5, r6, r7, pc}
-	.4byte 0xfffffc02
-	.4byte 0x0fffffff
-	.4byte 0x000007ff
-	.4byte 0xfffffc01
-	.4byte 0x00000000
-	.4byte 0x10000000
+	.space 0x8e
 	.4byte 0x00100000
 	.4byte 0x00100000
 	.4byte 0x00100000

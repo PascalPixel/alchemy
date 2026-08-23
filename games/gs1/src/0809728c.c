271f73e60a8fa6a7fa3a0d8dedd3c8cdf068c727
Pascal Pixel <x@pascalpixel.com>
2026-08-23 23:23:07 +0100

    ☀️ 50% – Name event object initialization

--- games/gs1/src/0809728c.c
+++ games/gs1/src/0809728c.c
@@ -37,49 +37,50 @@ void Func_08096cdc(void *object, s32 mode, s32 value);
 #define MESSAGE_NO      0x926
 #endif
 
+#define InitializeEventObject Func_0809728c
 void Func_0809728c(void)
 {
     s32 zero;
     s32 event_value;
-    s32 temp_r7;
-    void *temp_r5;
-    void *temp_r6;
-    void *temp_sl;
+    s32 event_index;
+    void *event_object;
+    void *event_state;
+    void *render_state;
 
-    temp_r6 = *(void **)WORK_CELL_ADDR;
-    temp_sl = *(void **)RENDER_CELL_ADDR;
+    event_state = *(void **)WORK_CELL_ADDR;
+    render_state = *(void **)RENDER_CELL_ADDR;
     zero = 0;
-    temp_r5 = *(void **)((u8 *)temp_r6 + 0x10);
-    event_value = (s32)(*(s16 *)((u8 *)(temp_r6) + 0x1C));
-    temp_r7 = event_value;
-    Func_08009080(temp_r5, 0x14);
-    M2C_FIELD(temp_r5, u32 *, 0x38) = (s32)*(s32 *)((u8 *)(temp_r5) + 8);
-    M2C_FIELD(temp_r5, s32 *, 0x3C) = (s32)M2C_FIELD(temp_r5, s32 *, 0xC);
-    M2C_FIELD(temp_r5, s32 *, 0x40) = (s32)M2C_FIELD(temp_r5, s32 *, 0x10);
-    M2C_FIELD(temp_r5, u32 *, 0x24) = 0;
-    M2C_FIELD(temp_r5, u32 *, 0x28) = 0;
-    *(s32 *)((u8 *)(temp_r5) + 0x2C) = 0;
-    if ((s8)M2C_FIELD(temp_r6, s8 *, 0x22) != 0) {
+    event_object = *(void **)((u8 *)event_state + 0x10);
+    event_value = (s32)(*(s16 *)((u8 *)(event_state) + 0x1C));
+    event_index = event_value;
+    Func_08009080(event_object, 0x14);
+    M2C_FIELD(event_object, u32 *, 0x38) = (s32)*(s32 *)((u8 *)(event_object) + 8);
+    M2C_FIELD(event_object, s32 *, 0x3C) = (s32)M2C_FIELD(event_object, s32 *, 0xC);
+    M2C_FIELD(event_object, s32 *, 0x40) = (s32)M2C_FIELD(event_object, s32 *, 0x10);
+    M2C_FIELD(event_object, u32 *, 0x24) = 0;
+    M2C_FIELD(event_object, u32 *, 0x28) = 0;
+    *(s32 *)((u8 *)(event_object) + 0x2C) = 0;
+    if ((s8)M2C_FIELD(event_state, s8 *, 0x22) != 0) {
         Func_080f9010(212);
-        M2C_FIELD(temp_r5, s32 *, 0x6C) = CALLBACK_1;
+        M2C_FIELD(event_object, s32 *, 0x6C) = CALLBACK_1;
     }
-    if ((s8)M2C_FIELD(temp_r6, s8 *, 0x23) != 0) {
-        Func_08096cdc(temp_r5, 1, 0);
-        Func_08015120((s32)temp_r7, 4);
-        if ((s8)M2C_FIELD(temp_r6, s8 *, 0x21) != 0) {
-            Func_08015040((void *)MESSAGE_NO, (s32)*(s8 *)((u8 *)(temp_r6) + 0x71C));
+    if ((s8)M2C_FIELD(event_state, s8 *, 0x23) != 0) {
+        Func_08096cdc(event_object, 1, 0);
+        Func_08015120((s32)event_index, 4);
+        if ((s8)M2C_FIELD(event_state, s8 *, 0x21) != 0) {
+            Func_08015040((void *)MESSAGE_NO, (s32)*(s8 *)((u8 *)(event_state) + 0x71C));
         } else {
-            Func_08015040((void *)MESSAGE_NO, (s32)*(s8 *)((u8 *)(temp_r6) + 0x71C));
+            Func_08015040((void *)MESSAGE_NO, (s32)*(s8 *)((u8 *)(event_state) + 0x71C));
         }
-        Func_08096cdc(temp_r5, 0, 0x10);
+        Func_08096cdc(event_object, 0, 0x10);
     }
     if (Func_080770c0(0x140) != 0) {
-        if ((s8)M2C_FIELD(temp_r6, s8 *, 0x22) != 0) {
-            M2C_FIELD(temp_r5, s32 *, 0x6C) = CALLBACK_2;
+        if ((s8)M2C_FIELD(event_state, s8 *, 0x22) != 0) {
+            M2C_FIELD(event_object, s32 *, 0x6C) = CALLBACK_2;
         }
-        Func_08009080(temp_r5, 0x15);
+        Func_08009080(event_object, 0x15);
     } else {
         Func_08097174();
     }
-    M2C_FIELD(temp_sl, s8 *, 0xCC7) = 1;
+    M2C_FIELD(render_state, s8 *, 0xCC7) = 1;
 }

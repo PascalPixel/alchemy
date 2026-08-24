e92ba8dfa1b1a50b27a31025b8e72102db578835
Pascal Pixel <x@pascalpixel.com>
2026-08-24 08:21:26 +0100

    ☀️ 50% – Humanize script object and battle pair helpers

--- games/gs1/src/battle/effects/run_effect.c
+++ games/gs1/src/battle/effects/run_effect.c
@@ -57,7 +57,7 @@ void BattleEffect_ResumeObject(s32 objectId);
 s32 Func_0808df1c(s32 objectId, s32 battle_mode);
 s32 Func_0809ae3c(s32 objectId);
 s32 Func_0808d5a4(s32 objectId);
-void Func_080970f8(s32 selected_object, s32 objectId);
+void BattleEffect_SetupObjectPair(s32 selected_object, s32 objectId);
 void Func_0809ab98(s32 objectId);
 void BattleEffect_PauseObject(s32 objectId);
 void Func_0809abb4(void);
@@ -118,7 +118,7 @@ void RunBattleEffect(void)
         objectId = Func_0808df1c(Data_02000240.selected_object, battle_mode);
         objectId = Func_0809ae3c(objectId);
         if (Func_0808d5a4(objectId) != 0) {
-            Func_080970f8(Data_02000240.selected_object, objectId);
+            BattleEffect_SetupObjectPair(Data_02000240.selected_object, objectId);
             Func_0809ab98(objectId);
             BattleEffect_PauseObject(objectId);
             Data_02000240.selected_id = objectId;
--- games/gs1/src/script/interpreter/apply_object_argument.c
+++ games/gs1/src/script/interpreter/apply_object_argument.c
@@ -1,9 +1,9 @@
 #include "object_commands.h"
 #include "script_interpreter.h"
 
-s32 Func_0800d8c4(struct ScriptInterpreter *interpreter)
+s32 Script_ApplyObjectArgument(struct ScriptInterpreter *interpreter)
 {
-    Func_0800c300(interpreter,
+    ObjectDispatch_ApplyArgumentToChildren(interpreter,
         interpreter->script[interpreter->cursor + 1]);
     interpreter->cursor = (u16)interpreter->cursor + 2;
     return 1;

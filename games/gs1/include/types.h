649865f87ab30edd9e62be34dea1afa99ff5bd8f
Pascal Pixel <x@pascalpixel.com>
2026-08-24 07:02:53 +0100

    ☀️ 50% – Humanize padded text label helper

--- games/gs1/include/types.h
+++ games/gs1/include/types.h
@@ -196,6 +196,7 @@ typedef int bool;
 #define UiText_ShowLocalizedMessageAndWait Func_08026fa8
 #define Text_FormatHex Func_08029274
 #define UiText_SetRenderString Func_08020b14
+#define UiText_DrawPaddedLabel Func_08020b64
 #define UiWork_FinalizePending Func_08015140
 #define UiWork_FinalizePendingCore Func_08019a54
 #define UiTimedNotice_Create Func_0801c34c

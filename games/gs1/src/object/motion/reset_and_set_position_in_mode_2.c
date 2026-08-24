0c09f9c953e26a8876e15b82cdbfe2016a83810e
Pascal Pixel <x@pascalpixel.com>
2026-08-24 09:17:22 +0100

    ☀️ 50% – Humanize reset-position helpers

--- games/gs1/src/object/motion/reset_and_set_position_in_mode_2.c
+++ games/gs1/src/object/motion/reset_and_set_position_in_mode_2.c
@@ -1,15 +1,17 @@
 #include "object_runtime.h"
 
 void Object_ResetMotion(struct ObjectRuntime *);
+void Object_SetMode(struct ObjectRuntime *, s32);
 void Object_SetPosition(struct ObjectRuntime *, s32, s32, s32);
 
-void Func_08092128(u32 object_id, s32 x, s32 z)
+void ObjectMotion_ResetAndSetPositionInMode2(u32 object_id, s32 x, s32 z)
 {
     struct ObjectRuntime *object = ObjectTable_Get(object_id);
 
     if (object != NULL) {
         object->movement_state = 0;
         Object_ResetMotion(object);
+        Object_SetMode(object, 2);
         Object_SetPosition(object, x << 16, object->y, z << 16);
     }
 }

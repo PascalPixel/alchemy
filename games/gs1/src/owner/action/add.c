b69f2e5c6703f17d7cf49bdb94721378c88e20c8
Pascal Pixel <x@pascalpixel.com>
2026-08-24 05:57:25 +0100

    ☀️ 50% – Humanize owner actions and UI window lifecycle

--- games/gs1/src/owner/action/add.c
+++ games/gs1/src/owner/action/add.c
@@ -13,7 +13,7 @@ struct State08078e28 {
 struct State08078e28 *Owner_GetState(s32);
 void Func_08078bf0(s32);
 
-s32 Func_08078e28(s32 state_index, s32 value)
+s32 OwnerAction_Add(s32 state_index, s32 value)
 {
     struct State08078e28 *state = Owner_GetState(state_index);
     s32 key = value & 0x3fff;

0eba4ab8dd3afc63666ab52ba37d37e15cbed3a6
Pascal Pixel <x@pascalpixel.com>
2026-08-24 09:13:30 +0100

    ☀️ 50% – Humanize object motion helpers

--- games/gs1/src/0809233c.c
+++ games/gs1/src/0809233c.c
@@ -4,7 +4,7 @@ void ObjectMotion_SetSpeedParameters(s32, s32, s32);
 struct ObjectRuntime *Object_GetById(u32);
 void ObjectMotion_SetHorizontalPositionWithTerrain(u32, s32, s32);
 void Object_SetMode(struct ObjectRuntime *, s32);
-void Func_0809228c(s32, s32, s32);
+void ObjectMotion_OffsetPositionAndResetMotion(s32, s32, s32);
 void Object_SetCallback(struct ObjectRuntime *, void *);
 
 extern s32 Data_02000240[];
@@ -23,7 +23,7 @@ void Func_0809233c(u32 arg0, s32 arg1, s32 arg2, s32 arg3)
             ObjectMotion_SetHorizontalPositionWithTerrain(arg0, other->x, other->z);
         object->movement_state = 0;
         Object_SetMode(object, 2);
-        Func_0809228c(arg0, arg1, arg2);
+        ObjectMotion_OffsetPositionAndResetMotion(arg0, arg1, arg2);
         Object_SetCallback(object, &Data_0809fbcc);
         object->action = arg3;
     }
--- games/gs1/src/object/motion/set_position_and_commit.c
+++ games/gs1/src/object/motion/set_position_and_commit.c
@@ -2,15 +2,16 @@
 
 void Object_ResetMotion(struct ObjectRuntime *);
 void Object_SetPosition(struct ObjectRuntime *, s32, s32, s32);
+void Object_CommitPosition(struct ObjectRuntime *);
 
-void Func_0809228c(u32 object_id, s32 x_offset, s32 z_offset)
+void ObjectMotion_SetPositionAndCommit(u32 object_id, s32 x, s32 z)
 {
     struct ObjectRuntime *object = ObjectTable_Get(object_id);
 
     if (object != NULL) {
         object->movement_state = 0;
         Object_ResetMotion(object);
-        Object_SetPosition(object, object->x + (x_offset << 16),
-            object->y, object->z + (z_offset << 16));
+        Object_SetPosition(object, x << 16, object->y, z << 16);
+        Object_CommitPosition(object);
     }
 }

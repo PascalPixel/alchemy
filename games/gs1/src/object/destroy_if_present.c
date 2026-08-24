e4cf6d34948b4463487274c3d99cc024f71be478
Pascal Pixel <x@pascalpixel.com>
2026-08-24 06:46:46 +0100

    ☀️ 50% – Humanize text and object runtime helpers

--- games/gs1/src/object/destroy_if_present.c
+++ games/gs1/src/object/destroy_if_present.c
@@ -2,7 +2,7 @@
 
 s32 Object_Destroy();
 
-void Func_0808f0c8(s32 arg0) {
+void Object_DestroyIfPresent(s32 arg0) {
     if (arg0 != 0) {
         Object_Destroy();
     }
--- games/gs1/src/object/motion/snap_to_terrain.c
+++ games/gs1/src/object/motion/snap_to_terrain.c
@@ -4,7 +4,7 @@
 
 s32 Func_080091a8(s32, s32, s32);
 
-void Func_0808e9a8(void *object) {
+void ObjectMotion_SnapToTerrain(void *object) {
     s32 angle;
 
     angle = Func_080091a8(0, M2C_FIELD(object, s32 *, 8), M2C_FIELD(object, s32 *, 0x10));

ca9c1328cc5770e8819eb697adb79cc3a3ed82ee
Pascal Pixel <x@pascalpixel.com>
2026-08-24 09:17:22 +0100

    ☀️ 50% – Humanize reset-position helpers

--- games/gs1/include/types.h
+++ games/gs1/include/types.h
@@ -432,6 +432,8 @@ typedef int bool;
 #define ObjectMotion_SnapToTerrain Func_0808e9a8
 #define ObjectMotion_SetPositionAndReset Func_080921c4
 #define ObjectMotion_SetPositionAndCommit Func_08092158
+#define ObjectMotion_ResetAndSetPosition Func_08092128
+#define ObjectMotion_ResetAndSetPositionInMode2 Func_0809218c
 #define ObjectMotion_OffsetPositionAndReset Func_080922c4
 #define ObjectMotion_OffsetPositionAndResetMotion Func_0809228c
 #define ObjectMotion_CommitPositionAndActivate Func_08092304
--- games/gs1/src/object/motion/reset_and_set_position.c
+++ games/gs1/src/object/motion/reset_and_set_position.c
@@ -1,17 +1,15 @@
 #include "object_runtime.h"
 
 void Object_ResetMotion(struct ObjectRuntime *);
-void Object_SetMode(struct ObjectRuntime *, s32);
 void Object_SetPosition(struct ObjectRuntime *, s32, s32, s32);
 
-void Func_0809218c(u32 object_id, s32 x, s32 z)
+void ObjectMotion_ResetAndSetPosition(u32 object_id, s32 x, s32 z)
 {
     struct ObjectRuntime *object = ObjectTable_Get(object_id);
 
     if (object != NULL) {
         object->movement_state = 0;
         Object_ResetMotion(object);
-        Object_SetMode(object, 2);
         Object_SetPosition(object, x << 16, object->y, z << 16);
     }
 }

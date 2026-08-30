8ccc235cea3e9282d9edbc2d3fbf7574f190dc53
Pascal Pixel <x@pascalpixel.com>
2026-08-30 21:05:08 +0100

    ☀️ 60% – Humanize UI icon locals

--- games/gs1/src/ui/icon/load_resource_into_slot.c
+++ games/gs1/src/ui/icon/load_resource_into_slot.c
@@ -5,16 +5,16 @@ s32 Resource_GetBuffer(s32 index, s32 value);
 void *Runtime_AllocateBlock(s32 arg0, s32 arg1);
 s32 Func_080153d0(s32, s32, s32 *, s32 *, s32);
 
-s32 UiIcon_LoadResourceIntoSlot(s32 arg0, s32 arg1) {
-    s32 sp4;
-    s32 sp8;
-    s32 temp_r5;
-    s32 temp_r5_2;
+s32 UiIcon_LoadResourceIntoSlot(s32 resource_id, s32 slot) {
+    s32 resource_info;
+    s32 selected_slot;
+    s32 buffer;
+    s32 allocation;
 
-    temp_r5_2 = Runtime_AllocateBlock(0x11, 0x608);
-    sp8 = arg1;
-    Func_080153d0(arg0, 0, &sp8, &sp4, 1);
-    temp_r5 = Resource_GetBuffer(arg1, temp_r5_2 + 0x400);
+    allocation = Runtime_AllocateBlock(0x11, 0x608);
+    selected_slot = slot;
+    Func_080153d0(resource_id, 0, &selected_slot, &resource_info, 1);
+    buffer = Resource_GetBuffer(slot, allocation + 0x400);
     Func_08002dd8(0x11);
-    return temp_r5;
+    return buffer;
 }

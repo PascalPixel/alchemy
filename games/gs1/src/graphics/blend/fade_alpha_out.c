591ab026c62e0c57d9715fc08caa9fa25e7a05ec
Pascal Pixel <x@pascalpixel.com>
2026-08-30 21:17:25 +0100

    ☀️ 60% – Humanize graphics utility locals

--- games/gs1/src/graphics/blend/fade_alpha_out.c
+++ games/gs1/src/graphics/blend/fade_alpha_out.c
@@ -3,16 +3,22 @@
 s32 WaitFrames(s32);
 void Graphics_FadeAlphaOut(void)
 {
-  s32 var_r5;
-  unsigned long long new_var;
-  long long new_var2;
-  new_var = 0x04000050;
-  *((s16 *) new_var) = 0x2044;
-  new_var2 = 0x04000052;
-  var_r5 = 1;
+  s32 alpha_step;
+  unsigned long long blend_control_address;
+  long long blend_alpha_address;
+  blend_control_address = 0x04000050;
+  *((s16 *) blend_control_address) = 0x2044;
+  blend_alpha_address = 0x04000052;
+  alpha_step = 1;
   do
   {
- do { *((s16 *) new_var2) = 0x1010 - var_r5; var_r5 += 2; WaitFrames(1); } while (0);
+    do
+    {
+      *((s16 *) blend_alpha_address) = 0x1010 - alpha_step;
+      alpha_step += 2;
+      WaitFrames(1);
+    }
+    while (0);
   }
-  while (var_r5 <= 0x10);
+  while (alpha_step <= 0x10);
 }

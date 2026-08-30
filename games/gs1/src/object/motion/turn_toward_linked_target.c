e517f4315f71cf1d56e19ff7023e9d98fb71e1d0
Pascal Pixel <x@pascalpixel.com>
2026-08-30 21:14:00 +0100

    ☀️ 60% – Humanize object motion locals

--- games/gs1/src/object/motion/turn_toward_linked_target.c
+++ games/gs1/src/object/motion/turn_toward_linked_target.c
@@ -5,26 +5,26 @@
 
 s32 ArcTan2(s32, s32);
 
-s32 ObjectMotion_TurnTowardLinkedTarget(void *arg0) {
-    s16 var_r0;
-    u16 temp_r3;
-    s32 temp;
-    void *temp_r1;
+s32 ObjectMotion_TurnTowardLinkedTarget(void *object) {
+    s16 turn_step;
+    u16 current_angle;
+    s32 target_angle;
+    void *linked_target;
 
-    temp_r1 = FIELD_AT_OFFSET(arg0, void *, 0x68);
-    if (temp_r1 != NULL) {
-        FIELD_AT_OFFSET(arg0, u8, 0x5A) = (u8) (0xFE & FIELD_AT_OFFSET(arg0, u8, 0x5A));
-        temp = (u16) ArcTan2(FIELD_AT_OFFSET(temp_r1, s32, 0x10) - FIELD_AT_OFFSET(arg0, s32, 0x10), FIELD_AT_OFFSET(temp_r1, s32, 8) - FIELD_AT_OFFSET(arg0, s32, 8));
-        temp_r3 = FIELD_AT_OFFSET(arg0, u16, 6);
-        var_r0 = temp - temp_r3;
-        if (var_r0 != 0) {
-            if (var_r0 > 0x1000) {
-                var_r0 = 0x1000;
+    linked_target = FIELD_AT_OFFSET(object, void *, 0x68);
+    if (linked_target != NULL) {
+        FIELD_AT_OFFSET(object, u8, 0x5A) = (u8) (0xFE & FIELD_AT_OFFSET(object, u8, 0x5A));
+        target_angle = (u16) ArcTan2(FIELD_AT_OFFSET(linked_target, s32, 0x10) - FIELD_AT_OFFSET(object, s32, 0x10), FIELD_AT_OFFSET(linked_target, s32, 8) - FIELD_AT_OFFSET(object, s32, 8));
+        current_angle = FIELD_AT_OFFSET(object, u16, 6);
+        turn_step = target_angle - current_angle;
+        if (turn_step != 0) {
+            if (turn_step > 0x1000) {
+                turn_step = 0x1000;
             }
-            if (var_r0 < -0x1000) {
-                var_r0 = -0x1000;
+            if (turn_step < -0x1000) {
+                turn_step = -0x1000;
             }
-            FIELD_AT_OFFSET(arg0, u16, 6) = (u16) (temp_r3 + var_r0);
+            FIELD_AT_OFFSET(object, u16, 6) = (u16) (current_angle + turn_step);
         }
     }
     return 1;

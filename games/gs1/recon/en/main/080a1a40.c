4b0f9333a58742da6d4f319e16bb962f84a5034f
Pascal Pixel <x@pascalpixel.com>
2026-08-31 08:12:03 +0100

    ☀️ 60% – Recover shared battle state lifetimes

--- games/gs1/recon/en/main/080a1a40.c
+++ games/gs1/recon/en/main/080a1a40.c
@@ -7,10 +7,9 @@ void BattleEffect_PositionSprite(s32 x_offset, s32 y_offset)
     u8 *state = *(u8 **)0x03001f2c;
     u8 *anchor = *(u8 **)(state + 16);
     u8 *sprite = *(u8 **)(state + 20);
-    u32 phase = (*(u32 *)0x03001e40 >> 1) & 7;
-    u16 x = ((u8 *)0x080af294)[phase] + x_offset +
+    u16 x = ((u8 *)0x080af294)[(*(volatile u32 *)0x03001e40 >> 1) & 7] + x_offset +
         (*(u16 *)(anchor + 12) * 8) + 8;
-    u16 y = ((u8 *)0x080af29d)[phase] + y_offset +
+    u16 y = ((u8 *)0x080af29d)[(*(volatile u32 *)0x03001e40 >> 1) & 7] + y_offset +
         (*(u16 *)(anchor + 14) * 8) + 8;
 
     *(u16 *)(sprite + 6) = x;

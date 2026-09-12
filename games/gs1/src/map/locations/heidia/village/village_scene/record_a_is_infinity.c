f3ebd5e486b6babbc091e404cae675ccaae39079
Pascal Pixel <x@pascalpixel.com>
2026-09-12 22:51:25 +0100

    ☀️ 51% – Consolidate Djinn management and floating-point status

--- games/gs1/src/map/locations/heidia/village/village_scene/record_a_is_infinity.c
+++
@@ -1,6 +0,0 @@
-#include "types.h"
-
-s32 SoftFloatRecordAIsInfinity(s32 *state)
-{
-    return *state == 4;
-}
--- games/gs1/src/map/locations/heidia/village/village_scene/record_c_is_zero.c
+++
@@ -1,6 +0,0 @@
-#include "types.h"
-
-s32 SoftFloatRecordCIsZero(s32 *state)
-{
-    return *state == 2;
-}

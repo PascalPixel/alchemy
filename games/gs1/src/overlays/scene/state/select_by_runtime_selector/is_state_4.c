585c12848a22a4c41e652d2ff0add4f035c85b37
Pascal Pixel <x@pascalpixel.com>
2026-09-13 03:41:54 +0100

    ☀️ 51% – Consolidate overlay soft-float runtime into one library module

--- games/gs1/src/overlays/scene/state/select_by_runtime_selector/is_state_4.c
+++
@@ -1,6 +0,0 @@
-#include "types.h"
-
-s32 SceneState_IsState4(s32 *state)
-{
-    return *state == 4;
-}
--- games/gs1/src/overlays/scene/state/select_by_runtime_selector/is_state_two_c.c
+++
@@ -1,6 +0,0 @@
-#include "types.h"
-
-s32 SceneState_IsStateTwoC(s32 *state)
-{
-    return *state == 2;
-}

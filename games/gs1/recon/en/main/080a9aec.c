e888076469971c0c2f01f4ca1659882e956b7b5a
Pascal Pixel <x@pascalpixel.com>
2026-09-13 06:40:34 +0100

    ☀️ 51% – Correct inventory category draft structure

--- games/gs1/recon/en/main/080a9aec.c
+++ games/gs1/recon/en/main/080a9aec.c
@@ -11,12 +11,15 @@ void Func_080a9aec(s32 window, const u16 *flags)
     const u16 *entry = flags;
 
     for (i = 14; i >= 0; i--) {
-        u16 value = *entry;
+        s32 value = *entry;
         entry++;
         if (value & 0x200) {
             s32 item_id = value & 0x1ff;
             u8 *info = Func_08077018(item_id);
             switch (info[2]) {
+            case 1:
+                Func_08015080(item_id + (s32)&Value_00000182, window, 8, 8);
+                break;
             case 2:
                 Func_08015080(item_id + (s32)&Value_00000182, window, 8, 56);
                 break;
@@ -25,9 +28,6 @@ void Func_080a9aec(s32 window, const u16 *flags)
                 break;
             case 4:
                 Func_08015080(item_id + (s32)&Value_00000182, window, 8, 24);
-                break;
-            case 1:
-                Func_08015080(item_id + (s32)&Value_00000182, window, 8, 8);
                 break;
             default:
                 break;

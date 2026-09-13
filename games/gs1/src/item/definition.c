b184bfe40f2699cc3632cc5930b8682f5b6c334a
Pascal Pixel <x@pascalpixel.com>
2026-09-13 12:52:34 +0100

    ☀️ 51% – Consolidate item and menu entry modules

--- games/gs1/src/item/definition.c
+++ games/gs1/src/item/definition.c
@@ -1,4 +1,11 @@
 #include "item.h"
+
+extern struct ItemDefinition Data_0807b6a8[];
+
+struct ItemDefinition *Item_GetDirect(s32 item_id)
+{
+    return Data_0807b6a8 + (item_id & 0x1ff);
+}
 #include "owner_state.h"
 
 s32 Item_CanOwnerEquipDirect(s32 owner_id, s32 item_id)
@@ -15,4 +22,12 @@ s32 Item_CanOwnerEquipDirect(s32 owner_id, s32 item_id)
         result &= 1;
     }
     return result;
+}
+
+s32 Item_IsCompatibleWithOwner(s32 owner_id, s32 item_id)
+{
+    if (Item_GetEquipmentGroup(item_id) == 0) {
+        return 1;
+    }
+    return Item_CanOwnerEquipDirect(owner_id, item_id);
 }
--- games/gs1/src/psynergy_menu/refresh_owner_entries_default.c
+++
@@ -1,9 +0,0 @@
-#include "types.h"
-
-void Func_080a1bdc(s32 arg0, s32 arg1, s32 arg2);
-void PsynergyMenu_RefreshOwnerEntriesDefault(void)
-{
-  int val;
-  val = 0x28;
-  Func_080a1bdc(0x6C, val, 8);
-}

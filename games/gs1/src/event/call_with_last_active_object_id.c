e99f3aa81c4e000c1c1183961ee1c950785ec827
Pascal Pixel <x@pascalpixel.com>
2026-09-13 13:50:31 +0100

    ☀️ 51% – Consolidate event object helpers and correct ID types

--- games/gs1/src/event/call_with_last_active_object_id.c
+++
@@ -1,9 +0,0 @@
-#include "types.h"
-
-s32 Func_0808b3ec(s32 event_id, s32 state);
-s32 ObjectTable_FindLastActiveId(void);
-
-void Event_CallWithLastActiveObjectId(s32 event_id)
-{
-    Func_0808b3ec(event_id, ObjectTable_FindLastActiveId());
-}

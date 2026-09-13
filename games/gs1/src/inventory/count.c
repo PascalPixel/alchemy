05f1eaaabd74a74e90d0a8a81e23c8467cca2f08
Pascal Pixel <x@pascalpixel.com>
2026-09-13 04:23:10 +0100

    ☀️ 51% – Consolidate inventory counting and party counters

--- games/gs1/src/inventory/count.c
+++ games/gs1/src/inventory/count.c
@@ -23,3 +23,21 @@ s32 Inventory_CountItem(s32 owner, s32 item_id)
     } while (index <= 14);
     return count;
 }
+
+s32 PartyInventory_CountItem(s32 item_id)
+{
+    u16 owners[16];
+    s32 item_count = 0;
+    s32 owner_count = Party_ListActiveOwners(owners);
+
+    if (item_count < owner_count) {
+        u16 *owner_cursor = owners;
+        s32 n = owner_count;
+
+        do {
+            item_count += Inventory_CountItem(*owner_cursor++, item_id);
+            n--;
+        } while (n != 0);
+    }
+    return item_count;
+}

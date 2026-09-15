226e28b5bf5ca1e81e826e305732789b2ce8c9ec
Pascal Pixel <x@pascalpixel.com>
2026-09-15 13:49:25 +0100

    ☀️ 52% – Recover Lunpa periodic leader effect as exact C

--- games/tbs/asm/overlays/resource_3ab_overlay.s
+++ games/tbs/asm/overlays/resource_3ab_overlay.s
@@ -564,63 +564,8 @@ AlchemyC_020011d0:
 	.space 0x70
 AlchemyC_02001240:
 	.space 0x64
-	push	{r5, r6, r7, lr}
-	mov	r7, r8
-	push	{r7}
-	movs	r0, #0
-	sub	sp, #28
-	bl	sub_02002cb0
-	ldr	r3, [pc, #100]
-	ldr	r7, [r3, #0]
-	movs	r3, #15
-	ands	r7, r3
-	mov	r8, r0
-	cmp	r7, #0
-	bne.n	.L_0200130c
-	bl	sub_02002bfa
-	movs	r3, #52
-	muls	r3, r0
-	lsrs	r3, r3, #16
-	lsls	r3, r3, #6
-	adds	r5, r3, #0
-	adds	r5, #230
-	adds	r0, r5, #0
-	bl	sub_02002c1c
-	add	r6, sp, #16
-	cmp	r0, #0
-	bge.n	.L_020012de
-	adds	r0, #3
-.L_020012de:
-	asrs	r3, r0, #2
-	adds	r0, r5, #0
-	str	r3, [r6, #0]
-	str	r7, [r6, #4]
-	bl	sub_02002c28
-	lsrs	r3, r0, #31
-	adds	r0, r0, r3
-	asrs	r0, r0, #1
-	str	r0, [r6, #8]
-	mov	r3, r8
-	ldr	r5, [r3, #8]
-	ldr	r4, [r6, #4]
-	ldr	r1, [r3, #12]
-	ldr	r2, [r3, #16]
-	ldr	r3, [r6, #0]
-	str	r0, [sp, #4]
-	adds	r0, r5, #0
-	str	r4, [sp, #0]
-	str	r7, [sp, #8]
-	str	r7, [sp, #12]
-	bl	sub_02001446
-.L_0200130c:
-	add	sp, #28
-	pop	{r3}
-	mov	r8, r3
-	pop	{r5, r6, r7}
-	pop	{r0}
-	bx	r0
-	.2byte 0x1e40
-	.2byte 0x0300
+AlchemyC_020012a4:
+	.space 0x78
 	push	{r5, r6, lr}
 	bl	sub_02002d00
 	movs	r1, #160
